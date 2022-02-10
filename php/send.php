897e4697a7f8e918602866a0fb41f70c07b58e9a
andrey-korchevniy <91483336+andrey-korchevniy@users.noreply.github.com>
2022-02-10 09:08:33 +0200

    заработала форма

--- php/send.php
+++ php/send.php
@@ -10,15 +10,15 @@ $email = trim(urldecode(htmlspecialchars($email)));
 $note = trim(urldecode(htmlspecialchars($note)));
 
 $myaddres  = "andrey.kor4evny@gmail.com";
- 
+
 /* А здесь прописывается текст сообщения, \n - перенос строки */
-$mes = "Тема: Заказ обратного звонка!\nИмя: $name \nТелефон: $phone \nПочта: $email \nСообщение: $note";
- 
+$mes = "Тема: Заказ c лендинга!\nИмя: $name \nТелефон: $phone \nПочта: $email \nСообщение: $note";
+
 /* А эта функция как раз занимается отправкой письма на указанный вами email */
-$sub='Заказ'; //сабж
-$email='Заказ обратного звонка'; // от кого
+$sub='Order from law-landing'; //сабж
+$email='landing'; // от кого
 $send = mail ($myaddres,$sub,$mes,"Content-type:text/plain; charset = utf-8\r\nFrom:$email");
- 
+
 ini_set('short_open_tag', 'On');
 header('Refresh: 3; URL=index.html');
 ?>
@@ -30,9 +30,9 @@ header('Refresh: 3; URL=index.html');
 <title>Дякуємо! Ми зв'яжемося с вами!</title>
 <meta name="generator">
 <script type="text/javascript">
-setTimeout('location.replace("/index.html")', 3000);
+setTimeout('location.replace("../index.html")', 3000);
 /*Изменить текущий адрес страницы через 3 секунды (3000 миллисекунд)*/
-</script> 
+</script>
 </head>
 <body>
 <h1>Дякуємо! Ми зв'яжемося с вами!</h1>
